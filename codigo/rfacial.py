e891c2b9469e6d00349c2244cfb68ca68f423263
Bernardo <bernardolr17@gmail.com>
2020-09-17 21:47:11 -0500

    new

--- codigo/rfacial.py
+++ codigo/rfacial.py
@@ -2,48 +2,31 @@ from cv2 import cv2
 import os
 import imutils 
 
-personName = 'Bernardo'
-dataPath = "C:/Users/Bernardo/Github/VS/Data"
-personPath = dataPath + '/' + personName
-#print(personPath)
-if not os.path.exists(personPath):
-    print('Carpeta creada: ', personPath)
-    os.makedirs(personPath)
 
+dataPath = 'C:/Users/Bernardo/Github/VS/Data' #Cambia a la ruta donde hayas almacenado Data
+imagePaths = os.listdir(dataPath)
+print('imagePaths=',imagePaths)
+
+#face_recognizer = cv2.face.EigenFaceRecognizer_create()
+#face_recognizer = cv2.face.FisherFaceRecognizer_create()
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+# Leyendo el modelo
+#face_recognizer.read('modeloEigenFace.xml')
+#face_recognizer.read('modeloFisherFace.xml')
+face_recognizer.read('modeloLBPHFace.xml')
+
+#cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
 cap = cv2.VideoCapture('VideoBernardo.mp4')
 
 faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
 
-count =0
-
 while True:
-    ret,frame = cap.read()
-    if ret == False: 
-        break
+	ret,frame = cap.read()
+	if ret == False: break
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	auxFrame = gray.copy()
 
-    frame = imutils.resize(frame,width=640)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    auxFrame = gray.copy()
-
-    faces = faceClassif.detectMultiScale(gray,1.3,5)
-
-    for (x,y,w,h) in faces:
-        cv2.rectangle(frame, (x,y),(x+w,+h),(0,255,0),2)
-        rostro = auxFrame[y:y+h,x:x+w]
-        rostro = cv2.resize(rostro,(150,150),interpolation= cv2.INTER_CUBIC)
-        cv2.imwrite(personPath + '/rostro_{}.jpg'.format(count),rostro)
-        count = count + 1
-
-    cv2.imshow('frame',frame)
-
-    k = cv2.waitKey(1)
-    if k == 27 or count >= 300:
-        break
-
-cap.release()
-cv2.destroyAllWindows()
-
-'''
 	faces = faceClassif.detectMultiScale(gray,1.3,5)
 
 	for (x,y,w,h) in faces:
@@ -52,7 +35,7 @@ cv2.destroyAllWindows()
 		result = face_recognizer.predict(rostro)
 
 		cv2.putText(frame,'{}'.format(result),(x,y-5),1,1.3,(255,255,0),1,cv2.LINE_AA)
-		
+		'''
 		# EigenFaces
 		if result[1] < 5700:
 			cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
@@ -68,8 +51,8 @@ cv2.destroyAllWindows()
 		else:
 			cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
 			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
-		
-    # LBPHFace
+		'''
+		# LBPHFace
 		if result[1] < 70:
 			cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
 			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
@@ -83,5 +66,4 @@ cv2.destroyAllWindows()
 		break
 
 cap.release()
-cv2.destroyAllWindows()    
-'''
+cv2.destroyAllWindows()
