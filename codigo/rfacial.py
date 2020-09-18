847d5bcbbe7799f465e67c46862b1279f6e232db
Bernardo <bernardolr17@gmail.com>
2020-09-17 21:50:11 -0500

    final

--- codigo/rfacial.py
+++ codigo/rfacial.py
@@ -1,69 +1,39 @@
-from cv2 import cv2
+import cv2
 import os
-import imutils 
+import imutils
 
 
-dataPath = 'C:/Users/Bernardo/Github/VS/Data' #Cambia a la ruta donde hayas almacenado Data
-imagePaths = os.listdir(dataPath)
-print('imagePaths=',imagePaths)
+name = "Carlos"
+dataPath = "data"
+namePath = dataPath +"/"+name
 
-#face_recognizer = cv2.face.EigenFaceRecognizer_create()
-#face_recognizer = cv2.face.FisherFaceRecognizer_create()
-face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+if not os.path.exists(namePath):
+    os.makedirs(namePath)
 
-# Leyendo el modelo
-#face_recognizer.read('modeloEigenFace.xml')
-#face_recognizer.read('modeloFisherFace.xml')
-face_recognizer.read('modeloLBPHFace.xml')
-
-#cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
-cap = cv2.VideoCapture('VideoBernardo.mp4')
-
+video = cv2.VideoCapture('video2Carlos.mp4')
 faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+count = 0
 
 while True:
-	ret,frame = cap.read()
-	if ret == False: break
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	auxFrame = gray.copy()
+    ret, frame = video.read()
+    if ret == False: break
+    frame = imutils.resize(frame, width=640)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    aux = frame.copy()
 
-	faces = faceClassif.detectMultiScale(gray,1.3,5)
+    faces = faceClassif.detectMultiScale(gray,1.3,5)
 
-	for (x,y,w,h) in faces:
-		rostro = auxFrame[y:y+h,x:x+w]
-		rostro = cv2.resize(rostro,(150,150),interpolation= cv2.INTER_CUBIC)
-		result = face_recognizer.predict(rostro)
+    for(x,y,w,h) in faces:
+        cv2.circle(frame,(x+int(w/2),y+int(h/2)),130,(0,0,0),2)
+        rostro = aux[y:y+h,x:x+w]
+        rostro = cv2.resize(rostro,(150,150),interpolation=cv2.INTER_CUBIC)
+        cv2.imwrite(namePath+'/rostro_{}.jpg'.format(count), rostro)
+        count +=1
+    cv2.imshow('frame',frame)
 
-		cv2.putText(frame,'{}'.format(result),(x,y-5),1,1.3,(255,255,0),1,cv2.LINE_AA)
-		'''
-		# EigenFaces
-		if result[1] < 5700:
-			cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
-			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
-		else:
-			cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
-			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
-		
-		# FisherFace
-		if result[1] < 500:
-			cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
-			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
-		else:
-			cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
-			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
-		'''
-		# LBPHFace
-		if result[1] < 70:
-			cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
-			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
-		else:
-			cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
-			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
-		
-	cv2.imshow('frame',frame)
-	k = cv2.waitKey(1)
-	if k == 27:
-		break
+    k=cv2.waitKey(1)
+    if k== 27 or count >= 300:
+        break
 
-cap.release()
+video.release()
 cv2.destroyAllWindows()
